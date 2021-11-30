1ed81ac81d47662add0e8935e7cf33ebabfb4c4d
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-11-30 16:50:41 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd 
+git clone https://github.com/ysfgrgO7/Dotfiles.git
+mkdir -p ~/.config
+cd ~/.config/
+cp -r ~/Dotfiles/.xprofile ~/.xprofile
+cp -r ~/Dotfiles/.config/alacritty/ ~/.config/alacritty
+cp -r ~/Dotfiles/.config/doom/ ~/.config/doom
+cp -r ~/Dotfiles/.config/picom/ ~/.config/picom
+cp -r ~/Dotfiles/.config/ranger/ ~/.config/ranger
+cp -r ~/Dotfiles/.config/rofi/ ~/.config/rofi
+cp -r ~/Dotfiles/.config/VSCodium/ ~/.config/VSCodium
+cp -r ~/Dotfiles/.config/zsh/ ~/.config/zsh/
+cp -r ~/Dotfiles/.local/share/bin/ ~/.local/share/bin
