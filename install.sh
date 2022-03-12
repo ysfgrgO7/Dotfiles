3079f7d39911b6c3c577149966541aeb4395662f
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-03-12 20:37:40 +0200

    adding the user.dirs

--- install.sh
+++ install.sh
@@ -10,4 +10,14 @@ cp -r ~/Dotfiles/.config/ranger ~/.config/ranger
 cp -r ~/Dotfiles/.config/rofi ~/.config/rofi
 cp -r ~/Dotfiles/.config/VSCodium ~/.config/VSCodium
 cp -r ~/Dotfiles/.config/zsh ~/.config/zsh
+cp -r ~/Dotfiles/.config/user-dirs.dirs ~/.config/user-dirs.dirs
 cp -r ~/Dotfiles/.local/share/bin/ ~/.local/share/bin
+
+mkdir ~/Media
+mv ~/Downloads ~/Media/dl
+mv ~/Documents ~/Media/doc
+mv ~/Music ~/Media/mus
+mv ~/Pictures ~/Media/pic
+mv ~/Videos ~/Media/vid
+mv ~/Templates ~/.local
+mv ~/Public ~/.local
