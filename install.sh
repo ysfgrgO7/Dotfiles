3db0b1fc5cd6698fb470eb181c98db7ab5ee3a14
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-09-30 17:53:10 +0200

    Installer

--- install.sh
+++ install.sh
@@ -4,11 +4,10 @@ mkdir -p ~/.config
 cd ~/.config/
 cp -r ~/Dotfiles/.xprofile ~/.xprofile
 cp -r ~/Dotfiles/.config/alacritty ~/.config/alacritty
-cp -r ~/Dotfiles/.config/doom ~/.config/doom
+cp -r ~/Dotfiles/.config/kitty ~/.config/kitty
 cp -r ~/Dotfiles/.config/picom ~/.config/picom
 cp -r ~/Dotfiles/.config/ranger ~/.config/ranger
 cp -r ~/Dotfiles/.config/rofi ~/.config/rofi
-cp -r ~/Dotfiles/.config/VSCodium ~/.config/VSCodium
 cp -r ~/Dotfiles/.config/zsh ~/.config/zsh
 cp -r ~/Dotfiles/.config/user-dirs.dirs ~/.config/user-dirs.dirs
 cp -r ~/Dotfiles/.local/share/bin/ ~/.local/share/bin
